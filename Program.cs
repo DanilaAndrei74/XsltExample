56d41c86d83742d970e9094a4e77a8044926cf78
Andrei Viorel DANILA <danilaandrei1234@gmail.com>
2023-01-07 12:50:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace XmlExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            var inputUrl = root + "InputFile.xml";
+            var xsltUrl = root + "XsltFile.xslt";
+            var outputUrl = root + "OutputFile.xml";
+
+            XPathDocument InputXml = new XPathDocument(inputUrl);
+            XslCompiledTransform Xslt = new XslCompiledTransform();
+            Xslt.Load(xsltUrl);
+            XmlTextWriter myWriter = new XmlTextWriter(outputUrl, null);
+            Xslt.Transform(InputXml, null, myWriter);
+
+        }
+    }
+}
